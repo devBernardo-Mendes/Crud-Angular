3c40ec62a5d31780cfa6554e72e9e1fc009ddc89
bernardo-mr1 <devbernardomr@gmail.com>
2021-08-11 16:51:41 -0300

    feat: impotted MatListModule and MatSidenavModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from'@angular/material/list';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +25,9 @@ import { NavComponent } from './components/template/nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
